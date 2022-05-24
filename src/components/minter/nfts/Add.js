3947b96795a1900af6986694e57a2d281b032e50
richiemikke <richardmichael0212@gmail.com>
2022-05-24 05:56:10 +0100

    initial commit

---
+++ src/components/minter/nfts/Add.js
@@ -0,0 +1,131 @@
+/* eslint-disable react/jsx-filename-extension */
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Button, Modal, Form, FloatingLabel } from "react-bootstrap";
+import { uploadToIpfs } from "../../../utils/minter";
+
+
+
+const AddNfts = ({ save, address }) => {
+  const [name, setName] = useState("");
+  const [ipfsImage, setIpfsImage] = useState("");
+  const [description, setDescription] = useState("");
+
+  
+  const [show, setShow] = useState(false);
+
+
+  // check if all form data has been filled
+  const isFormFilled = () =>
+      name && ipfsImage && description;
+
+  // close the popup modal
+  const handleClose = () => {
+    setShow(false);
+    setAttributes([]);
+  };
+
+  // display the popup modal
+  const handleShow = () => setShow(true);
+
+
+  return (
+    <>
+      <Button
+        onClick={handleShow}
+        variant="dark"
+        className="rounded-pill px-0"
+        style={{ width: "38px" }}
+      >
+        <i className="bi bi-plus"></i>
+      </Button>
+
+      {/* Modal */}
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Create NFT</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Form>
+            <FloatingLabel
+              controlId="inputLocation"
+              label="Name"
+              className="mb-3"
+            >
+              <Form.Control
+                type="text"
+                placeholder="Name of NFT"
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </FloatingLabel>
+
+            <FloatingLabel
+              controlId="inputDescription"
+              label="Description"
+              className="mb-3"
+            >
+              <Form.Control
+                as="textarea"
+                placeholder="description"
+                style={{ height: "80px" }}
+                onChange={(e) => {
+                  setDescription(e.target.value);
+                }}
+              />
+            </FloatingLabel>
+
+            <Form.Control
+              type="file"
+              className={"mb-3"}
+              onChange={async (e) => {
+                const imageUrl = await uploadToIpfs(e);
+                if (!imageUrl) {
+                  alert("failed to upload image");
+                  return;
+                }
+                setIpfsImage(imageUrl);
+              }}
+              placeholder="Product name"
+            ></Form.Control>
+            
+
+          </Form>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="outline-secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button
+            variant="dark"
+            disabled={!isFormFilled()}
+            onClick={() => {
+              save({
+                name,
+                ipfsImage,
+                description,
+                ownerAddress: address,
+            
+              });
+              handleClose();
+            }}
+          >
+            Create NFT
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+AddNfts.propTypes = {
+
+  // props passed into this component
+  save: PropTypes.func.isRequired,
+  address: PropTypes.string.isRequired,
+};
+
+export default AddNfts;
---
+++ src/components/minter/nfts/Card.js
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Card, Col, Badge, Stack, Row } from "react-bootstrap";
+import { truncateAddress } from "../../../utils";
+import Identicon from "../../ui/Identicon";
+
+const NftCard = ({ nft }) => {
+  const { image, description, owner, name, index } = nft;
+
+  return (
+    <Col key={index}>
+      <Card className=" h-100">
+        <Card.Header>
+          <Stack direction="horizontal" gap={2}>
+            <Identicon address={owner} size={28} />
+            <span className="font-monospace text-secondary">
+              {truncateAddress(owner)}
+            </span>
+            <Badge bg="secondary" className="ms-auto">
+              {index} ID
+            </Badge>
+          </Stack>
+        </Card.Header>
+
+        <div className=" ratio ratio-4x3">
+          <img src={image} alt={description} style={{ objectFit: "cover" }} />
+        </div>
+
+        <Card.Body className="d-flex  flex-column text-center">
+          <Card.Title>{name}</Card.Title>
+          <Card.Text className="flex-grow-1">{description}</Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
+
+NftCard.propTypes = {
+
+  // props passed into this component
+  nft: PropTypes.instanceOf(Object).isRequired,
+};
+
+export default NftCard;
